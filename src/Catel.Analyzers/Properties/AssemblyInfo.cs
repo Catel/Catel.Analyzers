8ee408aeb2382d06cfa10694902e9b93dbf4f494
Geert van Horrik <geert@catenalogic.com>
2019-07-24 16:17:02 +0200

    Create solution and projects

---
+++ src/Catel.Analyzers/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+// All other assembly info is defined in SolutionAssemblyInfo.cs
+
+[assembly: AssemblyTitle("Catel.Analyzers")]
+[assembly: AssemblyProduct("Catel.Analyzers")]
+[assembly: AssemblyDescription("Catel Roslyn analyzers")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: InternalsVisibleTo("Catel.Analyzers.Tests")]
